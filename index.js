8ba7ac4094f6ceda40742ceafa3cea213c4172c1
Margarita Masko <margaritam996@gmail.com>
2023-04-27 18:54:02 +0300

    feat: add 3 rows (en) keyboard

--- index.js
+++ index.js
@@ -263,3 +263,286 @@ let span28 = document.createElement("span")
 span28.className = "en";
 span28.innerHTML = "Del";
 divElement28.append(span28);
+
+//divRow3
+let divElement29 = document.createElement("div");
+divElement29.className = "key row__caps CapsLock";
+divRow3.append(divElement29);
+let span29 = document.createElement("span")
+span29.className = "en";
+span29.innerHTML = "CapsLock";
+divElement29.append(span29);
+
+let divElement30 = document.createElement("div");
+divElement30.className = "key KeyA";
+divElement29.after(divElement30);
+let span30 = document.createElement("span")
+span30.className = "en";
+span30.innerHTML = "a";
+divElement30.append(span30);
+
+let divElement31 = document.createElement("div");
+divElement31.className = "key KeyS";
+divElement30.after(divElement31);
+let span31 = document.createElement("span")
+span31.className = "en";
+span31.innerHTML = "s";
+divElement31.append(span31);
+
+let divElement32 = document.createElement("div");
+divElement32.className = "key KeyD";
+divElement31.after(divElement32);
+let span32 = document.createElement("span")
+span32.className = "en";
+span32.innerHTML = "d";
+divElement32.append(span32);
+
+let divElement33 = document.createElement("div");
+divElement33.className = "key KeyF";
+divElement32.after(divElement33);
+let span33 = document.createElement("span")
+span33.className = "en";
+span33.innerHTML = "f";
+divElement33.append(span33);
+
+let divElement34 = document.createElement("div");
+divElement34.className = "key KeyG";
+divElement33.after(divElement34);
+let span34 = document.createElement("span")
+span34.className = "en";
+span34.innerHTML = "g";
+divElement34.append(span34);
+
+let divElement35 = document.createElement("div");
+divElement35.className = "key KeyH";
+divElement34.after(divElement35);
+let span35 = document.createElement("span")
+span35.className = "en";
+span35.innerHTML = "h";
+divElement35.append(span35);
+
+let divElement36 = document.createElement("div");
+divElement36.className = "key KeyJ";
+divElement35.after(divElement36);
+let span36 = document.createElement("span")
+span36.className = "en";
+span36.innerHTML = "j";
+divElement36.append(span36);
+
+let divElement37 = document.createElement("div");
+divElement37.className = "key KeyK";
+divElement36.after(divElement37);
+let span37 = document.createElement("span")
+span37.className = "en";
+span37.innerHTML = "k";
+divElement37.append(span37);
+
+let divElement38 = document.createElement("div");
+divElement38.className = "key KeyL";
+divElement37.after(divElement38);
+let span38 = document.createElement("span")
+span38.className = "en";
+span38.innerHTML = "l";
+divElement38.append(span38);
+
+let divElement39 = document.createElement("div");
+divElement39.className = "key Semicolon";
+divElement38.after(divElement39);
+let span39 = document.createElement("span")
+span39.className = "en";
+span39.innerHTML = ";";
+divElement39.append(span39);
+
+let divElement40 = document.createElement("div");
+divElement40.className = "key Quote";
+divElement39.after(divElement40);
+let span40 = document.createElement("span")
+span40.className = "en";
+span40.innerHTML = "'";
+divElement40.append(span40);
+
+let divElement41 = document.createElement("div");
+divElement41.className = "key row__enter Enter";
+divElement40.after(divElement41);
+let span41 = document.createElement("span")
+span41.className = "en";
+span41.innerHTML = "Enter";
+divElement41.append(span41);
+
+//divRow4
+let divElement42 = document.createElement("div");
+divElement42.className = "key row__shift ShiftLeft";
+divRow4.append(divElement42);
+let span42 = document.createElement("span")
+span42.className = "en";
+span42.innerHTML = "Shift";
+divElement42.append(span42);
+
+let divElement43 = document.createElement("div");
+divElement43.className = "key KeyZ";
+divElement42.after(divElement43);
+let span43 = document.createElement("span")
+span43.className = "en";
+span43.innerHTML = "z";
+divElement43.append(span43);
+
+let divElement44 = document.createElement("div");
+divElement44.className = "key KeyX";
+divElement43.after(divElement44);
+let span44 = document.createElement("span")
+span44.className = "en";
+span44.innerHTML = "x";
+divElement44.append(span44);
+
+let divElement45 = document.createElement("div");
+divElement45.className = "key KeyC";
+divElement44.after(divElement45);
+let span45 = document.createElement("span")
+span45.className = "en";
+span45.innerHTML = "c";
+divElement45.append(span45);
+
+let divElement46 = document.createElement("div");
+divElement46.className = "key KeyV";
+divElement45.after(divElement46);
+let span46 = document.createElement("span")
+span46.className = "en";
+span46.innerHTML = "v";
+divElement46.append(span46);
+
+let divElement47 = document.createElement("div");
+divElement47.className = "key KeyB";
+divElement46.after(divElement47);
+let span47 = document.createElement("span")
+span47.className = "en";
+span47.innerHTML = "b";
+divElement47.append(span47);
+
+let divElement48 = document.createElement("div");
+divElement48.className = "key KeyN";
+divElement47.after(divElement48);
+let span48 = document.createElement("span")
+span48.className = "en";
+span48.innerHTML = "n";
+divElement48.append(span48);
+
+let divElement49 = document.createElement("div");
+divElement49.className = "key KeyM";
+divElement48.after(divElement49);
+let span49 = document.createElement("span")
+span49.className = "en";
+span49.innerHTML = "m";
+divElement49.append(span49);
+
+let divElement50 = document.createElement("div");
+divElement50.className = "key Comma";
+divElement49.after(divElement50);
+let span50 = document.createElement("span")
+span50.className = "en";
+span50.innerHTML = ",";
+divElement50.append(span50);
+
+let divElement51 = document.createElement("div");
+divElement51.className = "key Period";
+divElement50.after(divElement51);
+let span51 = document.createElement("span")
+span51.className = "en";
+span51.innerHTML = ".";
+divElement51.append(span51);
+
+let divElement52 = document.createElement("div");
+divElement52.className = "key Slash";
+divElement51.after(divElement52);
+let span52 = document.createElement("span")
+span52.className = "en";
+span52.innerHTML = "/";
+divElement52.append(span52);
+
+let divElement53 = document.createElement("div");
+divElement53.className = "key row__arrow ArrowUp";
+divElement52.after(divElement53);
+let span53 = document.createElement("span")
+span53.className = "en";
+span53.innerHTML = "▲";
+divElement53.append(span53);
+
+let divElement54 = document.createElement("div");
+divElement54.className = "key row__shift-right ShiftRight";
+divElement53.after(divElement54);
+let span54 = document.createElement("span")
+span54.className = "en";
+span54.innerHTML = "Shift";
+divElement54.append(span54);
+
+//divRow5
+let divElement55 = document.createElement("div");
+divElement55.className = "key row__ctrl ControlLeft";
+divRow5.append(divElement55);
+let span55 = document.createElement("span")
+span55.className = "en";
+span55.innerHTML = "Ctrl";
+divElement55.append(span55);
+
+let divElement56 = document.createElement("div");
+divElement56.className = "key row__meta MetaLeft";
+divElement55.after(divElement56);
+let span56 = document.createElement("span")
+span56.className = "en";
+span56.innerHTML = "Win";
+divElement56.append(span56);
+
+let divElement57 = document.createElement("div");
+divElement57.className = "key row__alt AltLeft";
+divElement56.after(divElement57);
+let span57 = document.createElement("span")
+span57.className = "en";
+span57.innerHTML = "Alt";
+divElement57.append(span57);
+
+let divElement58 = document.createElement("div");
+divElement58.className = "key row__space Space";
+divElement57.after(divElement58);
+let span58 = document.createElement("span")
+span58.className = "en";
+span58.innerHTML = " ";
+divElement58.append(span58);
+
+let divElement59 = document.createElement("div");
+divElement59.className = "key row__alt-right AltRight";
+divElement58.after(divElement59);
+let span59 = document.createElement("span")
+span59.className = "en";
+span59.innerHTML = "Alt";
+divElement59.append(span59);
+
+let divElement60 = document.createElement("div");
+divElement60.className = "key row__arrow ArrowLeft";
+divElement59.after(divElement60);
+let span60 = document.createElement("span")
+span60.className = "en";
+span60.innerHTML = "◀";
+divElement60.append(span60);
+
+let divElement61 = document.createElement("div");
+divElement61.className = "key row__arrow ArrowDown";
+divElement60.after(divElement61);
+let span61 = document.createElement("span")
+span61.className = "en";
+span61.innerHTML = "▼";
+divElement61.append(span61);
+
+let divElement62 = document.createElement("div");
+divElement62.className = "key row__arrow ArrowRight";
+divElement61.after(divElement62);
+let span62 = document.createElement("span")
+span62.className = "en";
+span62.innerHTML = "▶";
+divElement62.append(span62);
+
+let divElement63 = document.createElement("div");
+divElement63.className = "key row__ctrl-right Ctrl";
+divElement62.after(divElement63);
+let span63 = document.createElement("span")
+span63.className = "en";
+span63.innerHTML = "Ctrl";
+divElement63.append(span63);
